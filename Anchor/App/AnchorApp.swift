64764550cfdcaff8269c8ded9429d9fa0dc41c4e
Andrey Mokretsov <anmokretsov@gmail.com>
2026-08-24 01:44:29 +0300

    Add dummy window manager

--- Anchor/App/AnchorApp.swift
+++ Anchor/App/AnchorApp.swift
@@ -9,6 +9,8 @@ import SwiftUI
 
 @main
 struct AnchorApp: App {
+    private let windowManager = WindowManager()
+    
     var body: some Scene {
         Settings {
             EmptyView()
---
+++ Anchor/WindowManagement/WindowManager.swift
@@ -0,0 +1,12 @@
+//
+//  WindowManager.swift
+//  Anchor
+//
+//  Created by Андрей Мокрецов on 24.08.2026.
+//
+
+final class WindowManager {
+    init() {
+        print("Anchor started")
+    }
+}
